9126d549188468aac4f45e77d289c16ba3957b2f
Juri Kyoya <kyoyaj@gmail.com>
2019-05-12 19:22:33 -0400

    Deleting old proj

--- my-app2/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider, applyMiddleware, createStore} from "redux";
-import './index.css';
-import App from './App';
-
-//const store = createStore( bookState );
-ReactDOM.render( <App />, document.getElementById('app'));
-
